46b147778362f5dbdafd7677e5d760b536a0c927
André Rodríguez <andrerome@geekyducky.com>
2020-01-21 07:13:46 -0600

    Update Main.java

--- src/main/java/mx/tec/mining/Main.java
+++ src/main/java/mx/tec/mining/Main.java
@@ -1,15 +1,14 @@
 package mx.tec.mining;
 
-import org.deckfour.xes.model.XAttributeMap;
-import org.deckfour.xes.model.XEvent;
-import org.deckfour.xes.model.XLog;
-import org.deckfour.xes.model.XTrace;
+import org.deckfour.xes.model.*;
 import org.deckfour.xes.model.impl.XLogImpl;
 import org.deckfour.xes.out.XSerializer;
 import org.deckfour.xes.out.XesXmlSerializer;
 
 import java.io.FileOutputStream;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -17,12 +16,18 @@ public class Main {
         XLog log = XLogReader.openLog("simulation_logs.mxml.gz");
         for (XTrace trace: log) {
             Iterator<XEvent> it = trace.iterator();
+            Map<String, XAttribute> map = new HashMap<>();
             while (it.hasNext()) {
                 XEvent event = it.next();
                 XAttributeMap attributes = event.getAttributes();
-                if(attributes.containsKey("org:resource")){
-                    // System.out.println(attributes);
-                }else{
+                String label = attributes.get("concept:name").toString();
+                if(attributes.containsKey("org:resource")) {
+                    map.put(label, attributes.get("org:resource"));
+                }
+                if (map.containsKey(label)) {
+                    attributes.put("org:resource", map.get(label));
+                }
+                else{
                     it.remove();
                 }
             }
